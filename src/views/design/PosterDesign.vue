2d882ea7bdef2d43683ab065ec5711eae5fa1bd6
cailu <cailudys@gmail.com>
2023-11-12 23:46:30 +0800

    完成画布和拖拽功能

--- src/views/design/PosterDesign.vue
+++ src/views/design/PosterDesign.vue
@@ -1,49 +1,126 @@
 <template>
-  <div class="menu">
-    <div :key="1" :class="{ 'menu-item': true, active: 1 === activeItemId }" @click="selectItem(1)">
-      <div class="tabItem">
-        <apple-outlined />
-        <span>添加</span>
-      </div>
+  <div class="canvas-editor">
+    <div class="materials" v-for="material in materials" :key="material.id">
+      <img
+        :src="material.src"
+        :alt="material.name"
+        draggable="true"
+        @dragstart="handleDragStart($event, material)"
+      />
     </div>
-    <div :key="2" :class="{ 'menu-item': true, active: 2 === activeItemId }" @click="selectItem(2)">
-      <div class="tabItem">
-        <apple-outlined />
-        <span>模板</span>
-      </div>
+    <div
+      id="canvas-container"
+      @drop="handleDrop"
+      @dragover="handleDragOver"
+      style="position: relative; width: 800px; height: 600px"
+    >
+      <canvas id="canvas" width="800" height="600"></canvas>
     </div>
+    <button @click="console.log(canvas.toJSON())">打印数据</button>
+    <button @click="saveAsImage">保存画布图片</button>
+    <button @click="addText">添加文本</button>
   </div>
 </template>
 
-<script setup lang="ts">
-import { ref } from 'vue'
-import { AppleOutlined, AndroidOutlined } from '@ant-design/icons-vue'
+<script setup>
+import { onMounted, ref } from 'vue'
+import { fabric } from 'fabric'
 
-const activeItemId = ref(1) // 默认激活第一个菜单项
+const materials = [
+  { id: 1, name: 'Material 1', src: '/cat.png' },
+  { id: 2, name: 'Material 2', src: '/dog.png' }
+  // ... 更多素材
+]
 
-const selectItem = (id: number) => {
-  activeItemId.value = id
+const canvasRef = ref(null)
+let canvas
+
+const handleDragStart = (event, material) => {
+  event.dataTransfer.setData('material', JSON.stringify(material))
+}
+
+onMounted(() => {
+  canvas = new fabric.Canvas('canvas')
+  canvasRef.value = canvas
+})
+
+const handleDrop = (event) => {
+  event.preventDefault()
+  const material = JSON.parse(event.dataTransfer.getData('material'))
+  const rect = event.target.getBoundingClientRect()
+  const x = event.clientX - rect.left
+  const y = event.clientY - rect.top
+  fabric.Image.fromURL(material.src, (img) => {
+    img.set({
+      left: x,
+      top: y,
+      originX: 'center',
+      originY: 'center'
+    })
+    img.scaleToWidth(100) // 例如，调整素材的宽度为100px
+    canvas.add(img)
+    canvas.renderAll()
+  })
+}
+
+const handleDragOver = (event) => {
+  event.preventDefault()
+}
+
+const addText = () => {
+  const textbox = new fabric.IText('双击编辑文本', {
+    left: canvas.width / 2,
+    top: canvas.height / 2,
+    fontFamily: 'arial',
+    fill: '#333',
+    lineHeight: 1.1,
+    fontSize: 16,
+    originX: 'center',
+    originY: 'center'
+  })
+
+  canvas.add(textbox)
+  canvas.setActiveObject(textbox)
+  textbox.enterEditing()
+  textbox.selectAll()
+  canvas.renderAll()
+
+  // 监听“scaling”事件，以便文本在缩放时保持居中
+  textbox.on('scaling', function () {
+    textbox.set({ originX: 'center', originY: 'center' })
+  })
+}
+
+const saveAsImage = () => {
+  if (!canvas.backgroundColor) {
+    canvas.setBackgroundColor('white', canvas.renderAll.bind(canvas))
+  }
+
+  const dataURL = canvas.toDataURL({
+    format: 'png',
+    quality: 1
+  })
+  const downloadAnchorNode = document.createElement('a')
+  downloadAnchorNode.setAttribute('href', dataURL)
+  downloadAnchorNode.setAttribute('download', 'canvas-preview.png')
+  document.body.appendChild(downloadAnchorNode) // 需要为Firefox
+  downloadAnchorNode.click()
+  downloadAnchorNode.remove()
 }
 </script>
 
 <style scoped>
-.menu-item {
-  padding: 10px;
-  cursor: pointer;
-}
-
-.menu-item:hover {
-  background-color: #f0f0f0;
-}
-
-.active {
-  background-color: #e0e0e0;
-  color: #fff;
-}
-
-.tabItem {
+.canvas-editor {
   display: flex;
-  flex-direction: column;
-  align-items: center;
+}
+
+.materials {
+  width: 200px;
+  margin-right: 10px; /* 与画布之间的间隔 */
+}
+
+#canvas-container {
+  border: 1px solid black;
+  position: relative; /* 为了正确计算相对位置 */
 }
 </style>
